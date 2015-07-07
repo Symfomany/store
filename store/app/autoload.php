e16e27973f659ef38dd40210224a4cc98dbb8e33
Your Name <you@example.com>
2015-07-07 16:35:39 +0200

    add push

--- store/app/autoload.php
+++ store/app/autoload.php
@@ -6,8 +6,13 @@ use Composer\Autoload\ClassLoader;
 /**
  * @var ClassLoader $loader
  */
+
+date_default_timezone_set('Europe/Paris');
+
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+
 
 return $loader;
